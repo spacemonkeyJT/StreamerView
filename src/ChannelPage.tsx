d35c2e968fb2ba5fdea84dfed96e532bf70a5b46
spacemonkeyJT <spacemonkeyjt1@gmail.com>
2023-12-29 08:08:53 +0800

    Mobile support

--- src/ChannelPage.tsx
+++ src/ChannelPage.tsx
@@ -60,19 +60,19 @@ export default function ChannelPage({ channel, close, remove }: Props) {
       {isLive ? (
         <p>
           Playing <a target="_blank" href={channel.category_url}>{channel.game_name}</a> for {channel.viewers_desc} at <a target="_blank" href={channel.channel_url}>{channel.channel_url}</a><br />
-          {channel.content_classification_labels.map((tag, idx) => <span key={idx}><span className="tag">{tag}</span>&nbsp;</span>)}
+          {channel.content_classification_labels.map((tag, idx) => <span key={idx}><span className="tag">{tag}</span>{' '}</span>)}
         </p>
       ) : (
         <p>
           Last played <a target="_blank" href={channel.category_url}>{channel.game_name}</a> at <a target="_blank" href={channel.channel_url}>{channel.channel_url}</a><br />
-          {channel.content_classification_labels.map((tag, idx) => <span key={idx}><span className="tag">{tag}</span>&nbsp;</span>)}
+          {channel.content_classification_labels.map((tag, idx) => <span key={idx}><span className="tag">{tag}</span>{' '}</span>)}
         </p>
       )}
       <img className="profile-pic shadow" src={channel.profile_image_url} />
       <img className="thumbnail shadow" src={getSizedThumbnail(thumbUrl, 533, 300)} />
       <p className="stream-info">
         <span className="title">{channel.title}</span><br />
-        {channel.tags.map((tag, idx) => <span key={idx}><span className="tag">{tag}</span>&nbsp;</span>)}<br />
+        {channel.tags.map((tag, idx) => <span key={idx}><span className="tag">{tag}</span>{' '}</span>)}<br />
       </p>
       {/* {channel.language && <div className="language">Language: {channel.language.toLocaleUpperCase()}</div>} */}
       <p className="bio">
